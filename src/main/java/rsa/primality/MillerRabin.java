198bcb469d98d67cdf7049a66d70c8ca56536760
Marcio Geovani Jasinski <marciogj@gmail.com>
2014-09-11 21:06:19 -0300

    Algorithm revised and commented. I hope its fine now :)

--- src/main/java/rsa/primality/MillerRabin.java
+++ src/main/java/rsa/primality/MillerRabin.java
@@ -6,9 +6,10 @@ import java.util.Random;
 import rsa.api.PrimalityChecker;
 
 /**
- * The Miller–Rabin primality test or Rabin–Miller primality test is a primality test: an algorithm which determines whether a given number is prime.
- * 
- * Details: http://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+ * The Miller–Rabin primality test is a primality test: an algorithm which determines whether a given number is prime.
+ * Current implementation is based on following resources:
+ * - http://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+ * - http://www.ime.usp.br/~cris/aulas/06_2_5776/notas-de-aula/primos.pdf
  * @author marciogj
  *
  */
@@ -25,57 +26,72 @@ public class MillerRabin implements PrimalityChecker {
 		return test(t, ACCURACY);
 	}
 
+	/**
+	 * Evaluates if the input is a probable prime number. Although a true output does not guarantee that input is prime, the accuracy
+	 * parameter might give you some certainty. 
+	 * 
+	 * @param n A number N which will be tested as a probable prime
+	 * @param accuracy Number of times that Miller Rabin evaluation will be performed to increase the accuracy of the primality 
+	 * @return false if input number is definitely a composite (not prime); true if it is a probable prime (according to accuracy evaluation)
+	 */
 	public boolean test(BigInteger n, final int accuracy) {
-		System.out.println("n = " + n);
 		boolean isProbablePrime = true;
 		int loopAccuracy = 0;
 		while (isProbablePrime && loopAccuracy <= accuracy) {
-			System.out.println("accuracy " + loopAccuracy + "/" + accuracy);
 			isProbablePrime &= testMillerRabin(n);
 			if (!isProbablePrime) return false;
 			loopAccuracy++;
 		}
-
 		return isProbablePrime;
 	}
 
 	/**
-	 * Avalia se o numero N é possivelmente primo ou composto (não primo). 
-	 * @param n
-	 * @return
+	 * Perform a single evaluation of N  which increase around 1/4 of primality. 
+	 * @param A number N which will be tested as a probable prime
+	 * @return false if input number is definitely a composite (not prime); true if it is a probable prime (according to accuracy evaluation)
 	 */
 	private boolean testMillerRabin(final BigInteger n) {
 		BigInteger nMinusOne = n.subtract(BigInteger.ONE);
 		BigInteger a = BigInteger.ZERO;
-		//Obter um numero aleatório entre 1 e n-1. Ou seja:  1 < a < n-1
+		
+		//Step 1: Create a random number between 1 and n-1. Following property must be ensured:  1 < a < n-1
 		while (a.compareTo(BigInteger.ONE) <= 0 || a.compareTo(nMinusOne) >= 0) { 
 			a = new BigInteger(n.bitLength(), random);
-			System.out.println("a = " + a);
 		}
 
-		// n-1 = (2^s)*d
-		//s - é um inteiro qualquer 
-		//d - é um inteiro impar por definição (a parte par será de s)
-		// d = (n-1) / 2^s
+		//Step 2: Obtain 2 numbers according to the formula: n-1 = (2^s)*d, where:
+		//s - is a integer obtained from n-1 factoring: (n-1)/2/2/2... - we stop when division gives a float.    
+		//d - is a odd integer obtained to compete to equation d = (n-1) / 2^s - since n and s are already known
 		int s = factorOfTwo(nMinusOne);
 		BigInteger d = nMinusOne.divide(BigInteger.valueOf(2).pow(s));
-		System.out.println(nMinusOne + "  = 2^" + s + " * " + d);
-		 
+		
+		//Step 3: This step root is based on Euclids lemma and Fermat theorem that result in
+		//If a^d is not congruent to 1 (mod n) - then n is a composite
+		//If a^(2*2*...*2*d) is not congruent to -1 (mod n) - then n is a composite as well
+		//Otherwise the number seems to be a prime unlesse we have a liar number
 		BigInteger x = a.modPow(d, n); //x = a^d mod n
-		System.out.println("x = " + x);
-		//2^0
-		if (x.equals(BigInteger.ONE) || x.equals(nMinusOne)) return true; // deve gerar um novo a
+		
+		// Checks whether a^d is congruent to 1 (mod n) or x = n-1 which is congurent to -1 (mod n)
+		// If one of above rules matches, it´s a pseudo prime. New verification might be performed with another a
+		if (x.equals(BigInteger.ONE) || x.equals(nMinusOne)) return true; 
 
+		
+		// Performs a verification to find a witness of n composition unless we reach (n-1) which indicates a prime probability
+		// This evaluation also uses the definition that a^(2*2*...*2 * d) must be congruent to -1 (mod n)   
 		for (int i=0; i < s-1; i++) {
-			x = x.modPow(BigInteger.valueOf(2), n); //x^2 mod n
-			System.out.println("x = " + x);
-			if (x.equals(BigInteger.ONE)) return false; //é um numero composto
-			if (x.equals(nMinusOne)) return true; // deve gerar um novo a
+			x = x.modPow(BigInteger.valueOf(2), n); //x = x^(2*d) mod n
+			if (x.equals(BigInteger.ONE)) return false; //1 is NOT congruent to -1 (mod n) - This is a composite number
+			if (x.equals(nMinusOne)) return true; // (n-1) is congruent to -1 (mod n) - We have a prime chance
 		}
-
-		return false; //é um numero composto
+		return false; //it´s a composite since n-1 was not reached.
 	}
 
+	/**
+	 * Factor a number (using divisor number 2) while the division result is zero. 
+	 * 
+	 * @param m The number to be factored
+	 * @return The factor of 2 of input number. 
+	 */
 	private int factorOfTwo(final BigInteger m) {
 		BigInteger two = BigInteger.valueOf(2);
 		BigInteger factored = m;
--- tests/rsa/primality/test/MillerRabinTest.java
+++ tests/rsa/primality/test/MillerRabinTest.java
@@ -14,25 +14,44 @@ public class MillerRabinTest {
 	@Test
 	public void falsePrimalityTest() {
 		MillerRabin mr = new MillerRabin();
+		assertFalse("221 is prime?", mr.test(BigInteger.valueOf(221)) );
+		assertFalse("341 is prime?", mr.test(BigInteger.valueOf(341)) );
+		assertFalse(mr.test(new BigInteger("1298074214633706835075030044377088")));
 		
-		assertFalse("221 é primo?", mr.test(BigInteger.valueOf(221)) );
-		assertFalse("341 é primo?", mr.test(BigInteger.valueOf(341)) );
-		
-				
+		//http://en.wikipedia.org/wiki/Catalan_pseudoprime
+		assertFalse(mr.test(BigInteger.valueOf(5907)));
+		assertFalse(mr.test(BigInteger.valueOf(1194649)));
+		assertFalse(mr.test(BigInteger.valueOf(12327121)));
 	}
 	
-	
+	/**
+	 * 
+	 * http://www.bigprimes.net/archive/prime
+	 */
 	@Test
 	public void realPrimalityTest() {
 		MillerRabin mr = new MillerRabin();
-		//http://www.bigprimes.net/archive/prime
-		assertTrue("80737 é primo?", mr.test(BigInteger.valueOf(80737)) );
-		assertTrue("1020379 é primo?", mr.test(BigInteger.valueOf(1020379)) );
-		
-
-		
-		
+		assertTrue("2789 is prime?", mr.test(BigInteger.valueOf(2789)) );
+		assertTrue("80737 is prime?", mr.test(BigInteger.valueOf(80737)) );
+		assertTrue("1020379 is prime?", mr.test(BigInteger.valueOf(1020379)) );	
 				
+		
+		//Carol primes - http://en.wikipedia.org/wiki/List_of_prime_numbers
+		assertTrue(mr.test(BigInteger.valueOf(7)));
+		assertTrue(mr.test(BigInteger.valueOf(47)));
+		assertTrue(mr.test(BigInteger.valueOf(223)));
+		assertTrue(mr.test(BigInteger.valueOf(3967)));
+		assertTrue(mr.test(BigInteger.valueOf(16127)));
+		assertTrue(mr.test(BigInteger.valueOf(1046527)));
+		assertTrue(mr.test(BigInteger.valueOf(16769023)));
+		assertTrue(mr.test(BigInteger.valueOf(1073676287)));
+		assertTrue(mr.test(new BigInteger("68718952447")));
+		assertTrue(mr.test(new BigInteger("274876858367")));
+		assertTrue(mr.test(new BigInteger("4398042316799")));
+		assertTrue(mr.test(new BigInteger("1125899839733759")));
+		assertTrue(mr.test(new BigInteger("18014398241046527")));
+		assertTrue(mr.test(new BigInteger("1298074214633706835075030044377087")));
+		
 	}
 	
 }
