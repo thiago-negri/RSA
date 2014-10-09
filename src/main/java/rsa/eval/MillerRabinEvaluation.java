8b83c34904a95ffaf9297a2e314251da0abee4e9
Marcio Geovani Jasinski <marciogj@gmail.com>
2014-10-08 23:18:27 -0300

    Adicionado a compração do miller rabin.

---
+++ src/main/java/rsa/eval/MillerRabinEvaluation.java
@@ -0,0 +1,46 @@
+package rsa.eval;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.util.Random;
+
+import rsa.primality.MillerRabin;
+
+public class MillerRabinEvaluation {
+	private static MillerRabin millerRabin = new MillerRabin();
+	private static int accuracy = 50;
+	
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		int repetition = 10;
+		
+		int bits = 16;
+		Random random = new Random();
+		
+		PrintWriter writer = new PrintWriter("miller-rabin-"+System.currentTimeMillis()+".csv", "UTF-8");
+		
+		writer.println("bits;nanoseconds");
+		while (bits <= 1024) {
+			BigInteger number = BigInteger.probablePrime(bits, random);
+			int average = 0;
+			for(int i =0; i < repetition; i++) {
+				average += eval(number);
+			}
+			average = average/repetition;
+			System.out.println("Bits: "+ number.bitLength() + "\tAvg Time: " + average + " ns");
+			writer.println(number.bitLength()+";"+average);
+			bits += 16;
+		}
+		writer.close();
+		
+	}
+	
+	private static long eval(BigInteger n) {
+		long start = System.nanoTime();
+		boolean isPrime = millerRabin.test(n, accuracy);
+		long end = System.nanoTime();
+		return isPrime ? end - start : -1;
+	}
+
+}
