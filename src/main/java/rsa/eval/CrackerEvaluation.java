74ac6f942ca6a6db99107dc8a9611d5a859fc112
Marcio Geovani Jasinski <marciogj@gmail.com>
2014-10-14 23:27:56 -0300

    Adding CrackerEval

---
+++ src/main/java/rsa/eval/CrackerEvaluation.java
@@ -0,0 +1,54 @@
+package rsa.eval;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import rsa.api.Key;
+import rsa.api.RSACracker;
+import rsa.api.RSAKey;
+import rsa.api.RSAKeyGenerator;
+import rsa.api.RSAStrategy;
+import rsa.naive.NaiveRSAStrategy;
+
+public class CrackerEvaluation {
+
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
+		//int repetition = 100;
+
+		int bits = 16;
+		System.out.println("CrackerEvaluation");
+		PrintWriter writer = new PrintWriter("carcker-"+System.currentTimeMillis()+".csv", "UTF-8");
+
+		writer.println("bits;nanoseconds");
+
+	    final RSAStrategy strategy = new NaiveRSAStrategy();
+
+		bits = 16;
+		while (bits <= 32) {
+			System.out.println("Evaluating  "+ bits + " key"); 
+	        RSAKeyGenerator rsaKeyGenerator = strategy.rsaKeyGenerator(bits);
+	        RSAKey rsaKey = rsaKeyGenerator.next();
+	        Key publicKey = rsaKey.publicKey();
+
+		        RSACracker cracker = strategy.rsaCracker(publicKey.bitLength());
+		        long average = 0;
+		        //for(int i =0; i < repetition; i++) {
+		        	long start = System.nanoTime();
+		        	Key privateKey = cracker.findPrivateKeyOf(publicKey);
+		        	long end = System.nanoTime();
+		        	average += (end - start);
+		        	
+		        //}
+		        
+		        //average = average/repetition;
+				System.out.println("Bits: "+ bits + "\tAvg Time: " + average + " ns");
+				System.out.println("Private (" + privateKey.exponent() + ", " + privateKey.modulus()+ ")\nPublic (" + publicKey.exponent() + ", " + publicKey.modulus()+")\n\n");
+				writer.println(bits+";"+average);
+				bits += 1;
+		}
+		writer.close();
+
+	}
+
+}
