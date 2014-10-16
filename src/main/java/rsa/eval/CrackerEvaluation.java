8b176c636409dbeb03e19962c831a1be525c0289
Marcio Geovani Jasinski <marciogj@gmail.com>
2014-10-16 16:24:12 -0300

    Measuring.

--- src/main/java/rsa/eval/CrackerEvaluation.java
+++ src/main/java/rsa/eval/CrackerEvaluation.java
@@ -14,45 +14,65 @@ import rsa.naive.NaiveRSAStrategy;
 
 public class CrackerEvaluation {
 
-	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
-		//int repetition = 100;
+	private static final RSAStrategy strategy = new NaiveRSAStrategy();
 
-		int bits = 16;
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
+		int bits = 8;
 		System.out.println("CrackerEvaluation");
 		PrintWriter writer = new PrintWriter("cracker-"+System.currentTimeMillis()+".csv", "UTF-8");
-
 		writer.println("bits;nanoseconds");
 
-	    final RSAStrategy strategy = new NaiveRSAStrategy();
+		System.out.println("=============== Warming up =============");
+		warmup(32);
+		System.out.println("========================================");
 
-		bits = 16;
+		System.out.println("=============== Measuring =============");
 		while (bits <= 64) {
-		    RSAKeyGenerator rsaKeyGenerator = strategy.rsaKeyGenerator(bits);
-		    RSAKey rsaKey = rsaKeyGenerator.next();
+			RSAKeyGenerator rsaKeyGenerator = strategy.rsaKeyGenerator(bits);
+			RSAKey rsaKey = rsaKeyGenerator.next();
 			System.out.println("Evaluating  "+ rsaKey.privateKey().modulus().bitLength() + " key at " + new Date()); 
-	        Key publicKey = rsaKey.publicKey();
+			Key publicKey = rsaKey.publicKey();
 
-		        RSACracker cracker = strategy.rsaCracker();
-		        long average = 0;
-		        //for(int i =0; i < repetition; i++) {
-		        	long start = System.nanoTime();
-		        	Key privateKey = cracker.findPrivateKeyOf(publicKey);
-		        	long end = System.nanoTime();
-		        	average += (end - start);
-		        	
-		        //}
-		        
-		        //average = average/repetition;
-				System.out.println("\tAvg Time: " + average + " ns");
-				System.out.println("\tPrivate (" + privateKey.exponent() + ", " + privateKey.modulus()+ ")\n\tPublic (" + publicKey.exponent() + ", " + publicKey.modulus()+")\n\n");
-				
-				if (!(privateKey.exponent().equals(rsaKey.privateKey().exponent()) || !(privateKey.modulus().equals(rsaKey.privateKey().modulus()))))
-				        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Missed");
-				
-				writer.println(bits+";"+average);
-				bits += 1;
+			RSACracker cracker = strategy.rsaCracker();
+			long average = 0;
+			long start = System.nanoTime();
+			Key privateKey = cracker.findPrivateKeyOf(publicKey);
+			long end = System.nanoTime();
+			average += (end - start);
+			
+			System.out.println("\tAvg Time: " + average + " ns");
+			System.out.println("\tPrivate (" + privateKey.exponent() + ", " + privateKey.modulus()+ ")\n\tPublic (" + publicKey.exponent() + ", " + publicKey.modulus()+")\n\n");
+
+			if (!(privateKey.exponent().equals(rsaKey.privateKey().exponent()) || !(privateKey.modulus().equals(rsaKey.privateKey().modulus()))))
+				System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Missed");
+
+			writer.println(bits+";"+average);
+			bits += 2;
 		}
 		writer.close();
+
+	}
+
+	private static void warmup(int warmupBits) {
+		int bits = 8;
+		while (bits <= warmupBits) {
+			RSAKeyGenerator rsaKeyGenerator = strategy.rsaKeyGenerator(bits);
+			RSAKey rsaKey = rsaKeyGenerator.next();
+			System.out.println("Warming up  "+ rsaKey.privateKey().modulus().bitLength() + " key at " + new Date()); 
+			Key publicKey = rsaKey.publicKey();
+
+			RSACracker cracker = strategy.rsaCracker();
+			long average = 0;
+			long start = System.nanoTime();
+			Key privateKey = cracker.findPrivateKeyOf(publicKey);
+			long end = System.nanoTime();
+			average += (end - start);
+
+			System.out.println("\tAvg Time: " + average + " ns");
+			System.out.println("\tPrivate (" + privateKey.exponent() + ", " + privateKey.modulus()+ ")\n\tPublic (" + publicKey.exponent() + ", " + publicKey.modulus()+")\n\n");
+
+			bits += 2;
+		}
 
 	}
 
